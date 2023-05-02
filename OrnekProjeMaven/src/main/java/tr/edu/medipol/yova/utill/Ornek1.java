c6f13beef3f473be1a469dfee75bc83719a74374
cagricelik633 <cagricelik597@gmail.com>
2023-05-02 21:04:50 +0300

    dosyalar yüklendi

---
+++ OrnekProjeMaven/src/main/java/tr/edu/medipol/yova/utill/Ornek1.java
@@ -0,0 +1,13 @@
+package tr.edu.medipol.yova.utill;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Ornek1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(StringUtils.deleteWhitespace("   ab   c   "));
+        
+	}
+
+}
